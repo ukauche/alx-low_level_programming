5bec7020fa369e581b146e9a468b26b3f0abe31d
UkaUche <uchekaluka@gmail.com>
2023-06-17 16:40:46 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+  * Main - Entry point
+  * Return: 0 if successful
+  */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
