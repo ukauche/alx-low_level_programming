fd7ccc78e830d3e998882b02bb279e191a75efaf
UkaUche <uchekaluka@gmail.com>
2023-06-18 12:41:13 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+  * main -  a C program that prints exactly and that piece of art is useful"
+  * Return: 1 (Sucessful)
+  */
+int main(void)
+{
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	return (1);
+}
