742f651b0a08213f5eae1c7aabd6c65aec43d6c8
UkaUche <uchekaluka@gmail.com>
2023-06-19 11:20:27 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+  * main - Entry point
+  * Return: 0 if successful
+  */
+int main(void)
+{
+	int num = 0;
+while (num <= 9)
+{
+printf(num);
+num++;
+}
+printf('\n');
+return (0);
+}
