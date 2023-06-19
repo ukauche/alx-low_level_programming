582b07e4b65b9424ece9ba4a128196bbd2b5a8c6
UkaUche <uchekaluka@gmail.com>
2023-06-19 11:37:42 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio>
+/**
+  * main - Entry point
+  * Return: 0 if successful
+  */
+int main(void)
+{
+int num;
+while (num <= 9)
+{
+putchar("%d", num);
+num++;
+}
+putchar("\n");
+return (0);
+}
