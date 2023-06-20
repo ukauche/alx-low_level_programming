27d048c5928bce81e8fd24722aa30f96fb6d2b76
UkaUche <uchekaluka@gmail.com>
2023-06-20 15:41:54 -0700

    3-islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _islower - Checks for lowercase character
+ * Return: Always 0
+ */
+int _islower(int c)
+{
+if (c >= 97 && c <= 122)
+{
+return (1);
+}
+return (0);
+}
