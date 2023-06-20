23d1337ab44beb9d2af662501a444907f70bd24c
UkaUche <uchekaluka@gmail.com>
2023-06-20 14:57:23 -0700

    1-alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Entry point of code
+ * Return: Always 0
+ */
+
+void print_alphabet(void)
+{
+char ch;
+for (ch = 'a'; ch <= 'z'; ch++)
+_putchar(ch);
+_putchar('\n');
+}
