de6938741d425144b11056ee2e30b1a0b2b000a2
UkaUche <uchekaluka@gmail.com>
2023-06-22 19:39:14 -0700

    2-mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * mul - xplies two nos
+ * @a:- first nos
+ * @b:- second nos
+ * Return: 0
+ */
+
+int mul(int a, int b)
+{
+int result = a * b;
+return (result);
+}
