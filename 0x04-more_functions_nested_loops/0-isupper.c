43b0f2b62a132df60cf79f3f0380a34c0391d217
UkaUche <uchekaluka@gmail.com>
2023-06-22 19:24:35 -0700

    0-isupper and 1-isdigit

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _isupper - check for lower
+ * @c: char to be checked
+ * Return: 1 for uppercase & 0  for !=uppercase
+ */
+int _isupper(int c)
+{
+
+if (c >= 65 && c <= 90)
+{
+return (1);
+}
+return (0);
+}
