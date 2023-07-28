2b1558d2f72d1234546616429eafea07b94dc91b
UkaUche <uchekaluka@gmail.com>
2023-07-28 14:29:13 -0700

    101-hello_holberton.asm

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,39 +1,10 @@
-section .data
-    hello_string db "Hello, Holberton", 0xA ; 0xA is the ASCII code for a new line
+global   main
+	  extern    printf
+main:
+	  mov   edi, format
+	  xor   eax, eax
+	  call  printf
+	  mov   eax, 0
+	  ret
+format: db `Hello, Holberton\n`,0
 
-section .text
-    global _start
-
-_start:
-    ; syscall number for sys_write (1)
-    mov rax, 1
-
-    ; file descriptor for stdout (1)
-    mov rdi, 1
-
-    ; pointer to the string to print
-    mov rsi, hello_string
-
-    ; calculate the length of the string
-    xor rcx, rcx
-    mov rdx, 0
-.loop:
-    cmp byte [rsi + rcx], 0
-    je .end
-    inc rcx
-    jmp .loop
-.end:
-    ; length of the string to print
-    mov rdx, rcx
-
-    ; syscall to write the string to the console
-    syscall
-
-    ; syscall number for sys_exit (60)
-    mov rax, 60
-
-    ; exit code 0
-    xor rdi, rdi
-
-    ; syscall to exit the program
-    syscall
