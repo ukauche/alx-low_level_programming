4255b15c0b353123b2a5dcafe69b702ee825fc07
ukauche <uchekaluka@gmail.com>
2023-09-04 12:15:01 +0000

    3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,103 +1,67 @@
 #include "main.h"
 
-char *create_buffer(char *file);
-void close_file(int fd);
-
 /**
- * buffer - Allocates 1024 bytes for a buffer.
- * @file: The name of the file buffer is storing chars for.
+ * main - copies the content of one file to another
+ * @argc: argument count
+ * @argv: arguments passed
  *
- * Return: A pointer to the newly-allocated buffer.
+ * Return: 1 on success, exit otherwise
  */
-
-char *buffer(char *file)
+int main(int argc, char *argv[])
 {
-	char *fp;
+	int src, dest, n_read = 1024, wrote, close_src, close_dest;
+	unsigned int mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
+	char buffer[1024];
 
-	fp = malloc(sizeof(char) * 1024);
-
-	if (fp == NULL)
+	if (argc != 3)
 	{
-		dprintf(STDERR_FILENO,
-			"Error: Can't write to %s\n", file);
-		exit(99);
+		dprintf(STDERR_FILENO, "%s", "Usage: cp file_from file_to\n");
+		exit(97);
 	}
-
-	return (fp);
+	src = open(argv[1], O_RDONLY);
+	check_stats(src, -1, argv[1], 'O');
+	dest = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, mode);
+	check_stats(dest, -1, argv[2], 'W');
+	while (n_read == 1024)
+	{
+		n_read = read(src, buffer, sizeof(buffer));
+		if (n_read == -1)
+			check_stats(-1, -1, argv[1], 'O');
+		wrote = write(dest, buffer, n_read);
+		if (wrote == -1)
+			check_stats(-1, -1, argv[2], 'W');
+	}
+	close_src = close(src);
+	check_stats(close_src, src, NULL, 'C');
+	close_dest = close(dest);
+	check_stats(close_dest, dest, NULL, 'C');
+	return (0);
 }
 
 /**
- * close_file - Closes file descriptors.
- * @fd: File descriptor to be closed.
+ * check_stats - checks if a file can be opened or closed
+ * @stat: file descriptor of the file to be opened
+ * @filename: name of the file
+ * @mode: closing or opening
+ * @fd: file descriptor
+ *
+ * Return: void
  */
-void close_file_des(int fd)
+void check_stats(int stat, int fd, char *filename, char mode)
 {
-	int x;
-
-	x = close(fd);
-
-	if (x == -1)
+	if (mode == 'C' && stat == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
 		exit(100);
 	}
-}
-
-/**
- * main - Copies the contents of a file to another file.
- * @argc: Number of arguments supplied to the program.
- * @argv: Array of pointers to the arguments.
- *
- * Return: 0 on success.
- *
- * Description: If the argument count is incorrect - exit code 97.
- * If file_from does not exist or cannot be read - exit code 98.
- * If file_to cannot be created or written to - exit code 99.
- * If file_to or file_from cannot be closed - exit code 100.
- */
-int main(int argc, char *argv[])
-{
-	
-	char *fp;
-	int from, to, x, y;
-	
-	if (argc != 3)
+	else if (mode == 'O' && stat == -1)
 	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit(97);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", filename);
+		exit(98);
 	}
-
-	fp = create_buffer(argv[2]);
-	from = open(argv[1], O_RDONLY);
-	x = read(from, fp, 1024);
-	to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-
-	do {
-		if (from == -1 || x == -1)
-		{
-			dprintf(STDERR_FILENO,
-				"Error: Can't read from file %s\n", argv[1]);
-			free(fp);
-			exit(98);
-		}
-
-		y = write(to, fp, x);
-		if (to == -1 || y == -1)
-		{
-			dprintf(STDERR_FILENO,
-				"Error: Can't write to %s\n", argv[2]);
-			free(fp);
-			exit(99);
-		}
-
-		x = read(from, fp, 1024);
-		to = open(argv[2], O_WRONLY | O_APPEND);
-
-	} while (x > 0);
-
-	free(fp);
-	close_file(from);
-	close_file(to);
-
-	return (0);
+	else if (mode == 'W' && stat == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", filename);
+		exit(99);
+	}
 }
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,6 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdio.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
@@ -10,5 +11,8 @@
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+int _putchar(char c);
+int _strlen(char *str);
+void check_stats(int stat, int fd, char *filename, char mode);
 
 #endif
